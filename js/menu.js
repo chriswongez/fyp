9bec0fecf2830ade54e514da82ed13f3d91fb4e7
Chris Wong <chriswongez@gmail.com>
2021-01-24 18:05:46 +0800

    changes

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,8 @@
-setTimeout(() => {
-  document.getElementById("dinein").className += " opacityto1";
-}, 500);
+function start() {
+  setTimeout(() => {
+    document.getElementById("dinein").className += " opacityto1";
+  }, 500);
+}
 
 function goToBottom(id) {
   document.getElementById("dinein").style.top = "-100%";
@@ -24,5 +26,28 @@ function showMenu() {
     document.getElementById("menu").style.opacity = "1";
     document.getElementById("menu").style.visibility = "visible";
     document.getElementById("menu").style.marginTop = "80px";
+    document.getElementsByClassName("menu-title")[0].innerHTML =
+      "Please choose your food.";
   }, 200);
+}
+
+function hideFood() {
+  document.getElementsByClassName("food-menu-con")[0].style.opacity = "0";
+  document.getElementsByClassName("food-menu-con")[0].style.visibility =
+    "hidden";
+  document.getElementsByClassName("beve-menu-con")[0].style.display = "flex";
+  document.getElementById("select-beve").style.opacity = "0";
+
+  setTimeout(() => {
+    document.getElementsByClassName("menu-title")[0].innerHTML =
+      "Please choose your beverage.";
+    document.getElementById("select-beve").style.display = "none";
+    document.getElementsByClassName("beve-menu-con")[0].style.opacity = "1";
+    document.getElementsByClassName("beve-menu-con")[0].style.visibility =
+      "visible";
+  }, 200);
+
+  setTimeout(() => {
+    document.getElementsByClassName("food-menu-con")[0].style.display = "none";
+  }, 205);
 }
--- menu.php
+++ menu.php
@@ -1,39 +1,48 @@
+<script type="text/javascript" src="./js/menu.js"></script>
+
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Make Orders</title>
 
-    <link rel="stylesheet" href="./css/w3.css" />
-    <link rel="stylesheet" href="./css/style.css" />
-    <link rel="stylesheet" href="./css/menu.css" />
-  </head>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-  <body class="w3-khaki">
-    <?php include "./php/navbar.php";?>
-    <!-- dine in or take away prompt -->
-    <div id="dinein-con">
-      <div id="dinein" class="dinein w3-round-large w3-card">
-        <h1>Do you want to dine in or take away?</h1>
-        <div class="dinein-btn">
-          <button id="dine_in" class="w3-cyan" onclick="goToBottom(this.id);">
-            Dine in
-          </button>
-          <button id="take_away" class="w3-lime" onclick="goToBottom(this.id);">
-            Take away
-          </button>
-        </div>
+  <title>Make Orders</title>
+
+  <link rel="stylesheet" href="./css/w3.css" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="./css/menu.css" />
+</head>
+
+<body class="w3-khaki">
+  <?php include "./navbar.php"; ?>
+  <!-- dine in or take away prompt -->
+  <div id="dinein-con">
+    <div id="dinein" class="dinein w3-round-large w3-card">
+      <h1>Do you want to dine in or take away?</h1>
+      <div class="dinein-btn">
+        <button id="dine_in" class="w3-cyan" onclick="goToBottom(this.id);">
+          Dine in
+        </button>
+        <button id="take_away" class="w3-lime" onclick="goToBottom(this.id);">
+          Take away
+        </button>
       </div>
     </div>
-<div class="menu-wrapper">
-<div id="menu" class="menu w3-card">
-      <p class="menu-title">Please choose your food.</p>
-      <div class="menu-con">
+  </div>
+  <div class="menu-wrapper">
+    <div id="menu" class="menu w3-card">
+      <button id="select-beve" onclick="hideFood()">Select Beverage</button>
+      <p class="menu-title"></p>
+      <div id="food-menu-con" class="food-menu-con">
         <div class="food-con-wrapper">
           <div id="food_0001" class="food-con w3-card">
             <img src="./images/food-img.png" alt="" srcset="">
-            <p class="food-title" >Food title</p>
+            <p class="food-title">Food title</p>
             <p class="food-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
             <button class="btn-addtocart ">Add to cart</button>
           </div>
@@ -41,7 +50,7 @@
         <div class="food-con-wrapper">
           <div id="food_0001" class="food-con w3-card">
             <img src="./images/food-img.png" alt="" srcset="">
-            <p class="food-title" >Food title</p>
+            <p class="food-title">Food title</p>
             <p class="food-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
             <button class="btn-addtocart ">Add to cart</button>
           </div>
@@ -49,7 +58,7 @@
         <div class="food-con-wrapper">
           <div id="food_0001" class="food-con w3-card">
             <img src="./images/food-img.png" alt="" srcset="">
-            <p class="food-title" >Food title</p>
+            <p class="food-title">Food title</p>
             <p class="food-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
             <button class="btn-addtocart ">Add to cart</button>
           </div>
@@ -57,7 +66,7 @@
         <div class="food-con-wrapper">
           <div id="food_0001" class="food-con w3-card">
             <img src="./images/food-img.png" alt="" srcset="">
-            <p class="food-title" >Food title</p>
+            <p class="food-title">Food title</p>
             <p class="food-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
             <button class="btn-addtocart ">Add to cart</button>
           </div>
@@ -65,7 +74,7 @@
         <div class="food-con-wrapper">
           <div id="food_0001" class="food-con w3-card">
             <img src="./images/food-img.png" alt="" srcset="">
-            <p class="food-title" >Food title</p>
+            <p class="food-title">Food title</p>
             <p class="food-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
             <button class="btn-addtocart ">Add to cart</button>
           </div>
@@ -73,7 +82,7 @@
         <div class="food-con-wrapper">
           <div id="food_0001" class="food-con w3-card">
             <img src="./images/food-img.png" alt="" srcset="">
-            <p class="food-title" >Food title</p>
+            <p class="food-title">Food title</p>
             <p class="food-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
             <button class="btn-addtocart ">Add to cart</button>
           </div>
@@ -81,7 +90,7 @@
         <div class="food-con-wrapper">
           <div id="food_0001" class="food-con w3-card">
             <img src="./images/food-img.png" alt="" srcset="">
-            <p class="food-title" >Food title</p>
+            <p class="food-title">Food title</p>
             <p class="food-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
             <button class="btn-addtocart ">Add to cart</button>
           </div>
@@ -89,7 +98,79 @@
         <div class="food-con-wrapper">
           <div id="food_0001" class="food-con w3-card">
             <img src="./images/food-img.png" alt="" srcset="">
-            <p class="food-title" >Food title</p>
+            <p class="food-title">Food title</p>
+            <p class="food-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            <button class="btn-addtocart ">Add to cart</button>
+          </div>
+        </div>
+
+
+      </div>
+
+      <!-- beverage menu below -->
+
+
+      <div id="beve-menu-con" class="beve-menu-con">
+        <div class="food-con-wrapper">
+          <div id="food_0001" class="food-con w3-card">
+            <img src="./images/beve-img.png" alt="" srcset="">
+            <p class="food-title">Beverage Title</p>
+            <p class="food-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            <button class="btn-addtocart ">Add to cart</button>
+          </div>
+        </div>
+        <div class="food-con-wrapper">
+          <div id="food_0001" class="food-con w3-card">
+            <img src="./images/beve-img.png" alt="" srcset="">
+            <p class="food-title">Beverage Title</p>
+            <p class="food-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            <button class="btn-addtocart ">Add to cart</button>
+          </div>
+        </div>
+        <div class="food-con-wrapper">
+          <div id="food_0001" class="food-con w3-card">
+            <img src="./images/beve-img.png" alt="" srcset="">
+            <p class="food-title">Beverage Title</p>
+            <p class="food-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            <button class="btn-addtocart ">Add to cart</button>
+          </div>
+        </div>
+        <div class="food-con-wrapper">
+          <div id="food_0001" class="food-con w3-card">
+            <img src="./images/beve-img.png" alt="" srcset="">
+            <p class="food-title">Beverage Title</p>
+            <p class="food-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            <button class="btn-addtocart ">Add to cart</button>
+          </div>
+        </div>
+        <div class="food-con-wrapper">
+          <div id="food_0001" class="food-con w3-card">
+            <img src="./images/beve-img.png" alt="" srcset="">
+            <p class="food-title">Beverage Title</p>
+            <p class="food-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            <button class="btn-addtocart ">Add to cart</button>
+          </div>
+        </div>
+        <div class="food-con-wrapper">
+          <div id="food_0001" class="food-con w3-card">
+            <img src="./images/beve-img.png" alt="" srcset="">
+            <p class="food-title">Beverage Title</p>
+            <p class="food-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            <button class="btn-addtocart ">Add to cart</button>
+          </div>
+        </div>
+        <div class="food-con-wrapper">
+          <div id="food_0001" class="food-con w3-card">
+            <img src="./images/beve-img.png" alt="" srcset="">
+            <p class="food-title">Beverage Title</p>
+            <p class="food-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            <button class="btn-addtocart ">Add to cart</button>
+          </div>
+        </div>
+        <div class="food-con-wrapper">
+          <div id="food_0001" class="food-con w3-card">
+            <img src="./images/beve-img.png" alt="" srcset="">
+            <p class="food-title">Beverage Title</p>
             <p class="food-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
             <button class="btn-addtocart ">Add to cart</button>
           </div>
@@ -98,9 +179,10 @@
 
       </div>
     </div>
-</div>
+  </div>
 
-  </body>
+</body>
+
 </html>
 
 <script>
@@ -108,4 +190,17 @@
     document.getElementById("menu-btn").classList.add("active");
   };
 </script>
-<script type="text/javascript" src="./js/menu.js"></script>
+
+<?php
+//$_SESSION["cart"] = array("hi");
+unset($_SESSION["cart"]);
+if (!empty($_SESSION["cart"])) {
+  echo '<script type="text/javascript">',
+  'goToBottom("test");',
+  '</script>';
+} else {
+  echo '<script type="text/javascript">',
+  'start();',
+  '</script>';
+}
+?>
