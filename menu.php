af495496c2647713efde0c0842927bbae1e90eed
Chris Wong <chriswongez@gmail.com>
2021-01-25 01:08:06 +0800

    changes

--- menu.php
+++ menu.php
@@ -3,39 +3,49 @@
 <?php
 session_start();
 require_once('./php/dbconnect.php');
-if (isset($_POST['code']) && $_POST['code'] != "") {
-  $code = $_POST['code'];
-  $result = mysqli_query($con, "SELECT * FROM `products` WHERE `code`='$code'");
+//unset($_SESSION["cart"]);
+$status = "";
+if (isset($_POST['reset'])) {
+  unset($_SESSION["cart"]);
+}
+if (isset($_POST['food']) && $_POST['food'] != "") {
+  $foodCode = $_POST['food'];
+  $result = mysqli_query($con, "SELECT * FROM food WHERE foodCode ='$foodCode'");
   $row = mysqli_fetch_assoc($result);
-  $name = $row['name'];
-  $code = $row['code'];
-  $price = $row['price'];
-  $image = $row['image'];
+  $name = $row['foodName'];
+  $foodCode = $row['foodCode'];
+  $price = $row['foodPrice'];
+  // $image = $row['image'];
 
   $cartArray = array(
-    $code => array(
+    $foodCode => array(
       'name' => $name,
-      'code' => $code,
+      'foodCode' => $foodCode,
       'price' => $price,
       'quantity' => 1,
-      'image' => $image
+      // 'image' => $image
     )
   );
+  // $status = $foodCode;
 
-  if (empty($_SESSION["shopping_cart"])) {
-    $_SESSION["shopping_cart"] = $cartArray;
+  if (empty($_SESSION['cart'])) {
+    $_SESSION["cart"] = $cartArray;
     $status = "<div class='box'>Product is added to your cart!</div>";
   } else {
-    $array_keys = array_keys($_SESSION["shopping_cart"]);
-    if (in_array($code, $array_keys)) {
+    $array_keys = array_keys($_SESSION["cart"]);
+    $debug = implode("|", $array_keys);
+    if (in_array($foodCode, $array_keys)) {
       $status = "<div class='box' style='color:red;'>
 		Product is already added to your cart!</div>";
     } else {
-      $_SESSION["shopping_cart"] = array_merge($_SESSION["shopping_cart"], $cartArray);
-      $status = "<div class='box'>Product is added to your cart!</div>";
+
+      // $_SESSION["cart"] = array_merge($_SESSION["cart"], $cartArray);
+      $_SESSION["cart"] = $_SESSION["cart"] + $cartArray;
+      "<div class='box'>Product is added to your cart!</div>";
     }
   }
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -69,16 +79,24 @@ if (isset($_POST['code']) && $_POST['code'] != "") {
     </div>
   </div>
   <div class="menu-wrapper">
+
     <div id="menu" class="menu w3-card">
+      <?php print_r($_SESSION["cart"]); ?>
       <button id="select-beve" onclick="hideFood()">Select Beverage</button>
+      <form action="" method="post">
+        <input type="hidden" name="reset">
+        <input type="submit" value="reset">
+      </form>
       <p class="menu-title"></p>
+      <span><?php print_r($status) ?></span>
+      <span><?php print_r($debug) ?></span>
       <div id="food-menu-con" class="food-menu-con">
         <?php
         $result = mysqli_query($con, "SELECT * FROM `food`");
         while ($row = mysqli_fetch_assoc($result)) {
           echo "<form action='' method='post' class='food-con-wrapper'>
                 <div  class='food-con w3-card'>
-			          <input type='hidden' name=" . $row['foodCode'] . " value=" . $row['foodID'] . " />
+			          <input type='hidden' name='food' value=" . $row['foodCode'] . " />
 			          <img src='./images/food-img.png'>
                 <p class='food-title'>$" . $row['foodPrice'] . " " . $row['foodName'] . "</p>
                 <p class='food-desc'>" . $row['foodDescription'] . "</p>
@@ -119,7 +137,7 @@ if (isset($_POST['code']) && $_POST['code'] != "") {
 
 <?php
 //$_SESSION["cart"] = array("hi");
-unset($_SESSION["cart"]);
+//unset($_SESSION["cart"]);
 if (!empty($_SESSION["cart"])) {
   echo '<script type="text/javascript">',
   'goToBottom("test");',
