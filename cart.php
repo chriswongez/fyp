0e172d2ceca4cbf07b4d92ba9dcf59f8ee817ba9
Chris Wong <chriswongez@gmail.com>
2021-01-27 03:29:18 +0800

    changes

--- cart.php
+++ cart.php
@@ -24,7 +24,18 @@ if (isset($_POST['action']) && $_POST['action'] == "change") {
         }
     }
 }
+
+if (isset($_POST['proceed']) && !empty($_SESSION["cart"])) {
+    header('Location: ./checkout.php');
+} else if (isset($_POST['proceed']) && empty($_SESSION["cart"])) {
+    echo "<script>
+    alert('Your cart is empty! Please choose at least one item');
+    window.location.href='./menu.php';
+    </script>";
+}
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -32,7 +43,8 @@ if (isset($_POST['action']) && $_POST['action'] == "change") {
     <title>Food Ordering System</title>
     <link rel="stylesheet" href="./css/cart.css" />
     <link rel="stylesheet" href="css/style.css" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
@@ -42,9 +54,9 @@ if (isset($_POST['action']) && $_POST['action'] == "change") {
     <?php include "./navbar.php"; ?>
 
     <script>
-        window.onload = () => {
-            document.getElementById("cart-btn").classList.add("active");
-        };
+    window.onload = () => {
+        document.getElementById("cart-btn").classList.add("active");
+    };
     </script>
 
     <section id="home">
@@ -75,51 +87,59 @@ if (isset($_POST['action']) && $_POST['action'] == "change") {
                                 ?>
 
 
-                                    <!-- ----- -->
-                                    <tr>
-                                        <td>
-                                            <div class="main" style="position: relative;">
-                                                <div class="d-flex">
-                                                    <img src="./product/<?php echo $product["productImg"]; ?>" alt="" style="width:145px" style="height:98px">
-                                                </div>
-                                                <form method='post' style="position: absolute; right: 50px; top: 0;">
-                                                    <input type='hidden' name='code' value="<?php echo $product["productCode"]; ?>" />
-                                                    <input type='hidden' name='action' value="remove" />
-                                                    <button type='submit' class='remove'>Remove Item</button>
-                                                </form>
-                                                <div class="des">
-                                                    <p><?php echo $product["name"]; ?></p>
+                            <!-- ----- -->
+                            <tr>
+                                <td>
+                                    <div class="main" style="position: relative;">
+                                        <div class="d-flex">
+                                            <img src="./product/<?php echo $product["productImg"]; ?>" alt=""
+                                                style="width:145px" style="height:98px">
+                                        </div>
+                                        <form method='post' style="position: absolute; right: 50px; top: 0;">
+                                            <input type='hidden' name='code'
+                                                value="<?php echo $product["productCode"]; ?>" />
+                                            <input type='hidden' name='action' value="remove" />
+                                            <button type='submit' class='remove'>Remove Item</button>
+                                        </form>
+                                        <div class="des">
+                                            <p><?php echo $product["name"]; ?></p>
 
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <h6><?php echo "RM " . $product["price"]; ?></h6>
-                                        </td>
-                                        <td>
-                                            <div class="">
-                                                <form method='post' action=''>
-                                                    <input type='hidden' name='code' value="<?php echo $product["productCode"]; ?>" />
-                                                    <input type='hidden' name='action' value="change" />
-                                                    <select name='quantity' class='quantity' onchange="this.form.submit()">
-                                                        <option <?php if ($product["quantity"] == 1) echo "selected"; ?> value="1">1
-                                                        </option>
-                                                        <option <?php if ($product["quantity"] == 2) echo "selected"; ?> value="2">2
-                                                        </option>
-                                                        <option <?php if ($product["quantity"] == 3) echo "selected"; ?> value="3">3
-                                                        </option>
-                                                        <option <?php if ($product["quantity"] == 4) echo "selected"; ?> value="4">4
-                                                        </option>
-                                                        <option <?php if ($product["quantity"] == 5) echo "selected"; ?> value="5">5
-                                                        </option>
-                                                    </select>
-                                                </form>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <h6><?php echo "RM " . $product["price"] * $product["quantity"]; ?></h6>
-                                        </td>
-                                    </tr>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <h6><?php echo "RM " . $product["price"]; ?></h6>
+                                </td>
+                                <td>
+                                    <div class="">
+                                        <form method='post' action=''>
+                                            <input type='hidden' name='code'
+                                                value="<?php echo $product["productCode"]; ?>" />
+                                            <input type='hidden' name='action' value="change" />
+                                            <select name='quantity' class='quantity' onchange="this.form.submit()">
+                                                <option <?php if ($product["quantity"] == 1) echo "selected"; ?>
+                                                    value="1">1
+                                                </option>
+                                                <option <?php if ($product["quantity"] == 2) echo "selected"; ?>
+                                                    value="2">2
+                                                </option>
+                                                <option <?php if ($product["quantity"] == 3) echo "selected"; ?>
+                                                    value="3">3
+                                                </option>
+                                                <option <?php if ($product["quantity"] == 4) echo "selected"; ?>
+                                                    value="4">4
+                                                </option>
+                                                <option <?php if ($product["quantity"] == 5) echo "selected"; ?>
+                                                    value="5">5
+                                                </option>
+                                            </select>
+                                        </form>
+                                    </div>
+                                </td>
+                                <td>
+                                    <h6><?php echo "RM " . $product["price"] * $product["quantity"]; ?></h6>
+                                </td>
+                            </tr>
                             <?php
                                         if (empty($total_price)) {
                                             $total_price = 0;
@@ -156,6 +176,9 @@ if (isset($_POST['action']) && $_POST['action'] == "change") {
                     ?>
                 </li>
             </ul>
-            <a href="./checkout.php" class="proceed-btn"> Proceed to Checkout</a>
+            <form method="post" style="width: 100%; overflow: auto  ;">
+                <input type="submit" name="proceed" class="proceed-btn" value="Proceed to Checkout">
+            </form>
+
         </div>
     </div>
