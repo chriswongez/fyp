25272f31c61122b527c341711699e6b9e5d45697
Alvin Cheng <alvin_cheng2001@hotmail.com>
2021-01-29 00:52:09 +0800

    main

---
+++ Admin/Admin.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Admin </title>
+    <link rel="stylesheet" href="/Admin/css/admin.css" />
+    <script src="https://kit.fontawesome.com/08d8dbd162.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+
+    <div class="sidebar">
+        <div class="sidebar-header">
+            <h3 class="brand">
+                <span class="title">Foodie</span>
+                <i class="fas fa-bars"></i>
+            </h3>
+        </div>
+
+        <div class="sidebar-menu">
+            <ul>
+                <li>
+                    <a href="">
+                        <i class="fas fa-columns"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="">
+                        <i class="fas fa-hamburger"></i>
+                        <span>Product details</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="">
+                        <i class="fas fa-concierge-bell"></i>
+                        <span>Order details</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="">
+                        <i class="fas fa-user-circle"></i>
+                        <span>User Management</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="">
+                        <i class="fas fa-sign-out-alt"></i>
+                        <span>Log out</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="main-content">
+        <header>
+            <div>
+                <i class="fas fa-search"></i>
+                <input type="search" placeholder="search">
+            </div>
+
+            <div class="social-icons">
+                <i class="far fa-bell"></i>
+                <i class="far fa-comments"></i>
+                <div></div>
+            </div>
+        </header>
+
+        <main>
+            <h2>Dashboard</h2>
+
+            <div class="dashborad-cards">
+                <div class="card-single">
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+
+
+
+        </main>
+    </div>
+
+
+
+
+
+</body>
+
+</html>
