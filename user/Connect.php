a10e82c677a37407239a3b495c0c9d8db394d754
zhen chao <zhenchao2013@gmail.com>
2021-01-31 01:34:05 +0800

    new

--- user/Connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$Order_detail = filter_input（INPUT_POST，'Order_detail'）；
-$Order_date   = filter_input（INPUT_POST，'order_date'）；
-$Order_method = filter_input（INPUT_POST，'order_method'）；
-$Status       = filter_input（INPUT_POST，'status'）；
-$Amount_Paid  = filter_input（INPUT_POST，'amount_paid'）；
-
-?>
