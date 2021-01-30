e7998a3ff0c307948cb2453de0b541556f38c169
Chris Wong <chriswongez@gmail.com>
2021-01-30 18:57:44 +0800

    changes

--- Admin/orderdetails.php
+++ Admin/orderdetails.php
@@ -4,6 +4,18 @@ include_once("./templates/top.php");
 
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
+    $query = "SELECT * FROM orderhistory, users, product, orderitem where orderhistory.orderID = $id and orderhistory.userID = users.userID and orderhistory.orderID = orderitem.orderID and orderitem.productCode = product.productCode";
+    $result = mysqli_query($con, $query);
+    $numrow = mysqli_num_rows($result);
+    json_encode($row = mysqli_fetch_assoc($result));
+    $date = $row['orderDate'];
+    $username = $row['username'];
+    if ($row['orderMethod'] == 'selfc') {
+        $method = "Self Collect";
+    } else
+        $method = "Delivery";
+    $contact = $row['usercontact'];
+    $email = $row['useremail'];
 }
 ?>
 
@@ -28,11 +40,70 @@ if (isset($_GET['id'])) {
 
             <div class="row">
                 <div class="col-10">
-                    <h2>Orders Details for </h2>
+                    <h2>Orders Details for #<?php echo $id ?></h2>
                 </div>
             </div>
 
-            <div class="table-responsive">
+            <div class="container">
+                <div class="row">
+                    <div class="col-7">
+                        <table class="table table-bordered">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th>Product Code</th>
+                                    <th>Product Name</th>
+                                    <th>Product Quantity</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $query = "SELECT * FROM orderhistory, users, product, orderitem where orderhistory.orderID = $id and orderhistory.userID = users.userID and orderhistory.orderID = orderitem.orderID and orderitem.productCode = product.productCode";
+                                $result = mysqli_query($con, $query);
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    echo "<tr>
+                                    <td>" . $row['productCode'] . "</td>
+                                    <td>" . $row['productName'] . "</td>
+                                    <td>" . $row['quantity'] . "</td>
+                                </tr>";
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="col bg-light border rounded p-2 px-auto">
+                        <table class="table-sm  table-striped">
+                            <tr>
+                                <td>Order ID : </td>
+                                <td><?php echo $id ?></td>
+                            </tr>
+                            <tr>
+                                <td>Order Date : </td>
+                                <td><?php echo $date ?></td>
+                            </tr>
+                            <tr>
+                                <td>Order Method : </td>
+                                <td><?php echo $method ?></td>
+                            </tr>
+                            <tr>
+                                <td>Customer's Username : </td>
+                                <td><?php echo $username ?></td>
+                            </tr>
+                            <tr>
+                                <td>Customer's Contact : </td>
+                                <td><?php echo $contact ?></td>
+                            </tr>
+                            <tr>
+                                <td>Customer's Email : </td>
+                                <td><?php echo $email ?></td>
+                            </tr>
+
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- <div class="table-responsive">
                 <table class="table table-striped table-sm">
                     <thead>
                         <tr>
@@ -60,7 +131,7 @@ if (isset($_GET['id'])) {
                         ?>
                     </tbody>
                 </table>
-            </div>
+            </div> -->
             </main>
         </div>
     </div>
