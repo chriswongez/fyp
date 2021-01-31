5053f9061ced562a5686d5e27e83c5ada81605cf
Alvin Cheng <alvin_cheng2001@hotmail.com>
2021-01-31 11:44:33 +0800

    aeasw

--- Admin/index.php
+++ Admin/index.php
@@ -186,36 +186,7 @@ $self_cancelled = mysqli_num_rows(mysqli_query($con, "SELECT * FROM orderhistory
                         </div>
                         <div class="card-body py-0">
                             <div class="row">
-<<<<<<< HEAD
 
-=======
-                                <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-                                <script type="text/javascript">
-                                    google.charts.load('current', {
-                                        'packages': ['corechart']
-                                    });
-                                    google.charts.setOnLoadCallback(drawChart);
-
-                                    function drawChart() {
-
-                                        var data = google.visualization.arrayToDataTable([
-                                            ['Task', 'Number sales per Day'],
-                                            ['Received', 11],
-                                            ['Cancelled', 2],
-                                            ['Delivered', 2],
-
-                                        ]);
-
-                                        var options = {
-                                            title: 'Daily Activities'
-                                        };
-
-                                        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-                                        chart.draw(data, options);
-                                    }
-                                </script>
->>>>>>> ea61b04967b474a5556de586ba9bc6fee7b2a82a
                             </div>
 
                         </div>
