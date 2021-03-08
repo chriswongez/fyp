6873c685472c4898d906888f694f5e945656f351
Alvin Cheng <alvin_cheng2001@hotmail.com>
2021-03-09 07:30:36 +0800

    SASA

--- Admin/orderdetails.php
+++ Admin/orderdetails.php
@@ -70,6 +70,7 @@ if (isset($_GET['id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon">
+    <script src="https://kit.fontawesome.com/08d8dbd162.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <?php
